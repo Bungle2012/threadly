82142a0c4517914233b4586bf725a12fa560cc4b
Bungle2012 <jeffreyreeve101@gmail.com>
2016-05-12 15:39:23 +0100

    Final final

--- threadly/script.js
+++ threadly/script.js
@@ -1,6 +1,6 @@
 var main = function() {
   $('form').submit(function() {
-    var comment = $('#comment').val;
+    var comment = $('#comment').val();
     
     if(comment !== "") {
       var html = $('<li>').text(comment);
